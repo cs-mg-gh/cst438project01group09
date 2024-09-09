75ee50e2d8e76f6e7af28976052cd03560c9f582
cs-mg-gh <145803911+cs-mg-gh@users.noreply.github.com>
2024-09-08 21:28:54 -0700

    Made tabs with home view, location view, favorites view, and settings view  this is on xcode. features not functional

---
+++ p1cst438/p1cst438/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  p1cst438
+//
+//  Created by Matthew Garcia on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ p1cst438/p1cst438/p1cst438App.swift
@@ -0,0 +1,17 @@
+//
+//  p1cst438App.swift
+//  p1cst438
+//
+//  Created by Matthew Garcia on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct p1cst438App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

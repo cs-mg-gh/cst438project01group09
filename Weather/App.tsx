963fce48e398b2e421e2c407ac45f9fe80f8d16a
Diego Zavala <diezavala@csumb.edu>
2024-09-10 00:41:29 -0700

    Created database, works with create account

--- Weather/App.tsx
+++ Weather/App.tsx
@@ -5,18 +5,40 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import CreateAccountScreen from './screens/CreateAccountScreen';
+import DebugScreen from './screens/DebugScreen';
+import * as SQLite from 'expo-sqlite';
+import { useState, useEffect } from 'react';
+import { dbStart } from './db-folder/db-service';
+import { UserProvider } from './UserContext';
 
 const Stack = createNativeStackNavigator();
 
+
+
 export default function App() {
+  useEffect(()=>{
+    const startDB = async()=>{
+      try{
+        await dbStart();
+        console.log("Database started")
+      }catch(error){
+        console.log(`Error starting db: ${error}`);
+      }
+    };
+    startDB();
+  },[]);
+
   return (
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name='Home' component={HomeScreen}/>
-      <Stack.Screen name='Login' component={LoginScreen}/>
-      <Stack.Screen name='CreateAccount' component={CreateAccountScreen}/>
-    </Stack.Navigator>
-    </NavigationContainer>
+    <UserProvider>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name='Home' component={HomeScreen}/>
+        <Stack.Screen name='Login' component={LoginScreen}/>
+        <Stack.Screen name='CreateAccount' component={CreateAccountScreen}/>
+        <Stack.Screen name='Debug' component={DebugScreen}/>
+      </Stack.Navigator>
+      </NavigationContainer>
+    </UserProvider>
     
   );
 }
--- Weather/screens/HomeScreen.tsx
+++ Weather/screens/HomeScreen.tsx
@@ -1,11 +1,22 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import React, { useState, version } from 'react';
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useContext, useState, version } from 'react';
+import { UserContext } from "../UserContext";
 
 
 const HomeScreen = () => {
+
+    const userContext = useContext(UserContext);
+    if(!userContext){
+        throw new Error('User Context Error');
+    }
+    
+    const { username } = userContext;
+
+
     return(
         <View style={styles.container}>
             <Text>Welcome to the Home Screen!</Text>
+            <Text style={styles.welcomeText}>Welcome {username}</Text>
         </View>
     );
 };
@@ -16,5 +27,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    welcomeText:{
+        marginTop: 25,
+        fontSize: 26,
+        fontWeight: 'bold',
+    }
 })
 export default HomeScreen;

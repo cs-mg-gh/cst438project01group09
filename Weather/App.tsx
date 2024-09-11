85cfb2ccfb72e66f7deab6a101d65b62d9387513
LukeBerryPRO <Luke.Berry@proreferees.com>
2024-09-11 14:38:33 -0700

    Added navigation to Yesterday screen, api additions

--- Weather/App.tsx
+++ Weather/App.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import CreateAccountScreen from './screens/CreateAccountScreen';
+import YesterdayScreen from './screens/YesterdayScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,7 @@ export default function App() {
       <Stack.Screen name='Home' component={HomeScreen}/>
       <Stack.Screen name='Login' component={LoginScreen}/>
       <Stack.Screen name='CreateAccount' component={CreateAccountScreen}/>
+      <Stack.Screen name='Yesterday' component={YesterdayScreen}/>
     </Stack.Navigator>
     </NavigationContainer>
     
--- Weather/screens/HomeScreen.tsx
+++ Weather/screens/HomeScreen.tsx
@@ -1,20 +1,43 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import React, { useState, version } from 'react';
-
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
-    return(
+    const navigation = useNavigation();
+
+    const YesterdayButton = () =>{
+        navigation.navigate('Yesterday');
+    }
+
+    return (
         <View style={styles.container}>
             <Text>Welcome to the Home Screen!</Text>
+            <View style={styles.yesterdayButton}>
+            <TouchableOpacity onPress={YesterdayButton}>
+                <Text style={styles.buttonText}>Yesterday's Weather</Text>
+            </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-})
+    yesterdayButton: {
+        backgroundColor: 'blue',
+        padding: 10,
+        borderRadius: 5,
+        width: 200, 
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16, 
+    },
+});
+
 export default HomeScreen;
